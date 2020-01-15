c686f41bd0a5a353ad086a2f85ba00dfdc3c8da4
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-01-15 16:42:11 -0300

    Add the test frameworks enum.

---
+++ src/CSharpClassProject/CSharpClassProject.EmployeesRegister/Enums/TestFrameworksEnum.cs
@@ -0,0 +1,19 @@
+namespace CSharpClassProject.EmployeesRegister.Enums
+{
+    public enum TestFrameworksEnum
+    {
+        ///<summary>
+        /// 0 - SeleniumWebDriver
+        ///</summary>
+        SeleniumWebDriver,
+        ///<summary>
+        /// 1 - SeleniumIde
+        ///</summary>
+        SeleniumIde,
+        Cucumber,
+        NUnit,
+        XUnit,
+        MSTest,
+        JUnit
+    }
+}
