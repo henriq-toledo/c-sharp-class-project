b81dd7a228597842db0b2b83b1132cd11d406269
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-03-18 13:27:59 -0300

    Add database diagram and the connection string.

--- src/CSharpClassProject/CSharpClassProject.Ado/Classes/Data/Repository.cs
+++ src/CSharpClassProject/CSharpClassProject.Ado/Classes/Data/Repository.cs
@@ -5,7 +5,8 @@ namespace CSharpClassProject.Ado.Classes.Data
 {
     public abstract class Repository<TEntity>
         where TEntity : Employee
-    {
+    {        
+        protected string ConnectionString => "Server={serverName};Initial Catalog=Employees;Persist Security Info=False;User ID={userId};Password={userPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public abstract List<TEntity> Get { get; }
         public abstract SqlError Insert(TEntity entity);
         public abstract SqlError Update(TEntity entity);
