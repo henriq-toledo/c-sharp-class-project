4ec7b5a154e5cc89bf06023c326b20d2f414cde3
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-03-18 11:19:16 -0300

    Add the enum values to the programming languages enum.

--- src/CSharpClassProject/CSharpClassProject.Ado/Enums/ProgrammingLanguagesEnum.cs
+++ src/CSharpClassProject/CSharpClassProject.Ado/Enums/ProgrammingLanguagesEnum.cs
@@ -2,6 +2,12 @@ namespace CSharpClassProject.Ado.Enums
 {
     public enum ProgrammingLanguagesEnum
     {
-        
+        CPP,
+        CSharp,
+        Java,
+        Javascript,
+        Pascal,
+        R,
+        Python
     }
 }
