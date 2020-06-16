81345b363c02df6d79e9e017c413f8528e8df666
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-06-16 17:20:25 -0300

    Add the fields name and company name as required and set the string length to 60 and 100 respectively.

--- src/CSharpClassProject/CSharpClassProject.EfCore/Classes/Entities/Employee.cs
+++ src/CSharpClassProject/CSharpClassProject.EfCore/Classes/Entities/Employee.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CSharpClassProject.EfCore.Classes.Entities
 {
     public abstract class Employee
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(60)]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string CompanyName { get; set; }
     }
 }
