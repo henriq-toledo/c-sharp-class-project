1d1e1d6bfb2360d4e6153c32d6d6beccc33f5aa2
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-07-06 13:07:32 -0300

    Add the tester and developer CRUD.

--- src/CSharpClassProject/CSharpClassProject.EfCore/Program.cs
+++ src/CSharpClassProject/CSharpClassProject.EfCore/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CSharpClassProject.EfCore.Classes.Entities;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSharpClassProject.EfCore
 {
@@ -7,17 +9,16 @@ namespace CSharpClassProject.EfCore
     {
         static void Main(string[] args)
         {
-            // Get
-            using(var context = new Context())
-            {
-                foreach(var developer in context.Developers)
-                {
-                    Console.Write($"Id: {developer.Id}, Name: {developer.Name}, Company Name:{developer.CompanyName}");
-                }
-            }
+            //DeveloperCRUD();
+            TesterCRUD();
 
+            Console.ReadLine();
+        }
+
+        static void DeveloperCRUD()
+        {
             // Insert
-            using(var context = new Context())
+            using (var context = new Context())
             {
                 var developer = new Developer()
                 {
@@ -29,7 +30,83 @@ namespace CSharpClassProject.EfCore
                 context.SaveChanges();
             }
 
-            Console.ReadLine();
+            // Get
+            using (var context = new Context())
+            {
+                foreach (var developer in context.Developers)
+                {
+                    Console.Write($"Id: {developer.Id}, Name: {developer.Name}, Company Name:{developer.CompanyName}");
+                }
+            }
+
+            // Update
+            using (var context = new Context())
+            {
+                var developer = context.Developers.FirstOrDefault();
+
+                developer.CompanyName = "CC Corp.";
+
+                context.Entry(developer).State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+
+            // Delete
+            using (var context = new Context())
+            {
+                var developer = context.Developers.AsEnumerable().LastOrDefault();
+
+                context.Developers.Remove(developer);
+
+                context.SaveChanges();
+            }
+        }
+
+        static void TesterCRUD()
+        {
+            // Insert
+            using (var context = new Context())
+            {
+                var tester = new Tester()
+                {
+                    Name = "Ash",
+                    CompanyName = "Toei"
+                };
+
+                context.Testers.Add(tester);
+                context.SaveChanges();
+            }
+
+            // Get
+            using (var context = new Context())
+            {
+                foreach (var tester in context.Testers)
+                {
+                    Console.Write($"Id: {tester.Id}, Name: {tester.Name}, Company Name:{tester.CompanyName}");
+                }
+            }
+
+            // Update
+            using (var context = new Context())
+            {
+                var tester = context.Testers.FirstOrDefault();
+
+                tester.CompanyName = "CC Corp.";
+
+                context.Entry(tester).State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+
+            // Delete
+            using (var context = new Context())
+            {
+                var tester = context.Testers.AsEnumerable().LastOrDefault();
+
+                context.Testers.Remove(tester);
+
+                context.SaveChanges();
+            }
         }
     }
 }
