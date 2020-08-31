047ff66f57d70cc95185c7c3491d8096ff61124f
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-08-31 19:11:00 -0300

    Create the ref and out project.

---
+++ src/CSharpClassProject/CSharpClassProject.RefOutSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpClassProject.RefOutSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
