79edaf7987747ce4fc05a84bd68efd8d36303cb6
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-09-15 08:15:21 -0300

    Add the action and function sample project.

---
+++ src/CSharpClassProject/CSharpClassProject.ActionAndFunctionSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpClassProject.ActionAndFunctionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Action and Function!");
+            Console.ReadLine();
+        }
+    }
+}
