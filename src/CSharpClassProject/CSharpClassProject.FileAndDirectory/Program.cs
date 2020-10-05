9df1c479f734625d65fd4d549b8b01760c2bd272
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-10-05 19:32:27 -0300

    Add the file and directory project.

---
+++ src/CSharpClassProject/CSharpClassProject.FileAndDirectory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpClassProject.FileAndDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Files and Directories!");
+            Console.ReadLine();
+        }
+    }
+}
