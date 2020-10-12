bb35d36e10331ef7bcc95bb480c87cbd344412e4
Henrique Toledo <henriq_toledo@yahoo.com.br>
2020-10-12 17:21:48 -0300

    Add the async sample project.

---
+++ src/CSharpClassProject/CSharpClassProject.AsyncSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpClassProject.AsyncSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Async!");
+            Console.ReadLine();
+        }
+    }
+}
